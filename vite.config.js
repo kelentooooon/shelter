e47eeaf9d2f102068c90a664ce4036c655c2f2ef
Kelentooooon <ehapm5678@gmail.com>
2025-01-04 21:16:36 +0200

    Resolved merge conflicts

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,13 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  watch: {
+    usePolling: true,
+    interval: 100,
+  },
+  plugins: [react(
+    {
+      include: ['**/*.jsx', '**/*.tsx'],
+    }
+  )],
 })
