b7c29a19f4d51bb9b9eac87e5bf08cd4fb538560
Kelentooooon <ehapm5678@gmail.com>
2025-01-07 01:48:51 +0200

    Your commit message

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,26 @@ import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Menu from "./components/Menu";
 import Home from "./components/Home";
-import Loading from "./components/Loading"; 
+import Loading from "./components/Loading";
 import { loadMedia } from "./utils/loadMedia";
+import i from "./assets/img/big-silder/big1.jpg";
 
 const mediaUrls = [
   // Add the URLs of all your media files here
-  "/path/to/image1.jpg",
-  "/path/to/image2.jpg",
-  "/path/to/video1.mp4",
+  "./assets/img/big-silder/big1.jpg",
+  "./assets/img/big-silder/big2.jpg",
+  "./assets/img/big-silder/big3.jpg",
+  "./assets/img/big-silder/big4.jpg",
+  "./assets/img/big-silder/big5.jpg",
+  "./assets/img/big-silder/big6.jpg",
+  "./assets/img/big-silder/big7.jpg",
+  "./assets/img/big-silder/big8.jpg",
+  "./assets/img/big-silder/big9.jpg",
+  "./assets/img/big-silder/big10.jpg",
+  "./assets/img/hero.jpg",
+  "./assets/img/logo.png",
+  "./assets/video.mp4",
+
   // Add more media URLs as needed
 ];
 
--- src/utils/loadMedia.jsx
+++ src/utils/loadMedia.jsx
@@ -2,10 +2,24 @@ export const loadMedia = (mediaUrls) => {
     return Promise.all(
       mediaUrls.map((url) => {
         return new Promise((resolve, reject) => {
-          const img = new Image();
-          img.src = url;
-          img.onload = resolve;
-          img.onerror = reject;
+          if (url.endsWith('.mp4')) {
+            // Handle video files
+            fetch(url)
+              .then((response) => {
+                if (response.ok) {
+                  resolve();
+                } else {
+                  reject(new Error('Failed to load video'));
+                }
+              })
+              .catch(reject);
+          } else {
+            // Handle image files
+            const img = new Image();
+            img.src = url;
+            img.onload = resolve;
+            img.onerror = reject;
+          }
         });
       })
     );
